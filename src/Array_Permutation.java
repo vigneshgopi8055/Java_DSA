ac8ca7db204a24a8911e7d38e1e74219bb01c20b
Vignesh <vigneshgopi74@gmail.com>
2023-03-11 20:31:28 +0530

    Array permutation

---
+++ src/Array_Permutation.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.Arrays;
+
+//Build Array from Permutation
+public class Array_Permutation {
+    public static void main(String[] args) {
+        int []nums = {0,2,1,5,3,4};
+
+        System.out.println(Arrper(nums));
+
+    }
+    static int[] Arrper(int []nums){
+        final int n = nums.length;
+
+        for (int i = 0; i < n; ++i)
+            nums[i] += n * (nums[nums[i]] % n);
+
+        for (int i = 0; i < n; ++i)
+            nums[i] /= n;
+
+        return nums;
+    }
+
+}
