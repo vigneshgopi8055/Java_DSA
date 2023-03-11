00e206495eb202a05b5fdefa17cc39f1123c7d40
Vignesh <vigneshgopi74@gmail.com>
2023-03-11 21:09:45 +0530

    Array 2d

---
+++ src/Array_2d.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.util.Arrays;
+
+public class Array_2d {
+    public static void main(String[] args) {
+        int [][]arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        for (int i = 0 ; i< arr.length;i++){
+
+            System.out.println(Arrays.toString(arr[i]));
+
+        }
+    }
+}
