7266e076351a7aba83d02bd0091df619625f7053
Vignesh <vigneshgopi74@gmail.com>
2023-03-14 15:07:41 +0530

    leet code-268

---
+++ src/Missing_Number.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.util.Arrays;
+
+public class Missing_Number {
+    public static void main(String[] args) {
+        int []arr = {0,1};
+
+        System.out.println(sort(arr));
+    }
+    static int sort(int []nums){
+        int arr[] = new int[nums.length + 1];
+        for(int i=0;i<nums.length;i++)
+        {
+            arr[nums[i]] = 1;
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i] == 0)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
