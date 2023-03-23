9c95b40727cff030a406d286bba1d25295c3a4f9
Vignesh <vigneshgopi74@gmail.com>
2023-03-23 10:34:43 +0530

    fibonacci using recursion

---
+++ src/fibonacci_recursion.java
@@ -0,0 +1,13 @@
+package src;
+
+public class fibonacci_recursion {
+    public static void main(String[] args) {
+        System.out.println(fibo(7));
+    }
+    static int fibo(int n){
+        if (n == 0 || n==1){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
