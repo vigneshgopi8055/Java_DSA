e7f03bcd6d68001e2fb886ae1ba6c09527b38269
Vignesh <vigneshgopi74@gmail.com>
2023-03-23 10:02:27 +0530

    Recursion example

--- src/Recursion_2.java
+++ src/Recursion_2.java
@@ -5,22 +5,10 @@ public class Recursion_2 {
         print1(1);
     }
     static void print1(int n){
+        if (n == 5){
+            return;
+        }
         System.out.println(n);
-        print2(2);
-    }
-    static void print2(int n){
-        System.out.println(n);
-        print3(3);
-    }
-    static void print3(int n){
-        System.out.println(n);
-        print4(4);
-    }
-    static void print4(int n){
-        System.out.println(n);
-        print5(5);
-    }
-    static void print5(int n){
-        System.out.println(n);
+        print1(n+1);
     }
 }
