ce7dd1a848df3b7b5a05c3e05a340e956ccc2797
Vignesh <vigneshgopi74@gmail.com>
2023-03-27 22:00:21 +0530

    recursion sum from  n to 1

---
+++ src/Recursion_Sum_1toN.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.Scanner;
+
+public class Recursion_Sum_1toN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+    }
+    static int sum(int n){
+        if (n <= 0){
+            return 1;
+        }
+        return n + sum(n-1);
+    }
+}
