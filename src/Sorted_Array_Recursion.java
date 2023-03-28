cf156e1edbbeca96532e9589d300470db7180da4
Vignesh <vigneshgopi74@gmail.com>
2023-03-28 20:35:53 +0530

    sorted array using recursion

---
+++ src/Sorted_Array_Recursion.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Sorted_Array_Recursion {
+    public static void main(String[] args) {
+        int []arr = {1,2,3,4,5};
+        System.out.println(sorted(arr,0));
+    }
+    static boolean sorted(int []arr , int index){
+        if (index == arr.length-1)
+            return true;
+
+        return arr[index]<arr[index+1] && sorted(arr,index+1);
+    }
+}
