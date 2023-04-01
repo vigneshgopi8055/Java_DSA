3e113ffcb163679973441e9a4e5ca600a24e33c1
Vignesh <vigneshgopi74@gmail.com>
2023-04-01 11:56:51 +0530

    product of digits

--- src/Product_Of_Digits.java
+++ src/Product_Of_Digits.java
@@ -1,14 +1,14 @@
 package src;
 
-public class Sum_Of_digits {
+public class Product_Of_Digits {
     public static void main(String[] args) {
         int n = 1234;
         System.out.println(sum(n));
     }
     static int sum(int n){
         if (n == 0){
-            return 0;
+            return 1;
         }
-        return (n%10) + sum(n/10);
+        return (n%10) * sum(n/10);
     }
 }
