70abd656a84334fac8975fca9c6dee949ed67022
Vignesh <vigneshgopi74@gmail.com>
2023-04-10 12:05:18 +0530

    reversing array using recursion

---
+++ src/Array_Reverse_Recursion.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.Arrays;
+
+public class Array_Reverse_Recursion {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverse(arr,0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverse(int []arr , int start , int end){
+        if (start == end)
+            return;
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverse(arr,start+1,end-1);
+    }
+}
