8e0784da88aa3ac616b87f36494b54a1e92ce650
Vignesh <vigneshgopi74@gmail.com>
2023-04-11 11:54:13 +0530

    subsequence of a string

---
+++ src/SubSeq_Of_String.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubSeq_Of_String {
+    static List<String> al = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String s = "abc";
+        findsubsequences(s, ""); // Calling a function
+        System.out.println(al);
+    }
+    private static void findsubsequences(String s,
+                                         String ans) {
+        if (s.length() == 0) {
+            al.add(ans);
+            return;
+        }
+
+        // We add adding 1st character in string
+        findsubsequences(s.substring(1), ans + s.charAt(0));
+
+        // Not adding first character of the string
+        // because the concept of subsequence either
+        // character will present or not
+        findsubsequences(s.substring(1), ans);
+    }
+}
