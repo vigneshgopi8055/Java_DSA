903f8d9746946eb3e02122fdf921b9ea5f576c54
Vignesh <vigneshgopi74@gmail.com>
2023-04-12 22:18:38 +0530

    bubble sort using recursion

--- src/Bubble_Q1.java
+++ src/Bubble_Q1.java
@@ -5,21 +5,22 @@ import java.util.Arrays;
 public class Bubble_Q1 {
     public static void main(String[] args) {
         int []arr = {6,2,11,7,5};
-        sort(arr);
+        sort(arr,0,1);
         System.out.println(Arrays.toString(arr));
     }
-    static int sort(int []nums){
+    static void sort(int []nums , int index1 , int index2){
+        if (index1<0 || index2>nums.length-1)
+            return;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length-i; j++) {
-                if (nums[j] < nums[j-1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j-1];
-                    nums[j-1] = temp;
-                }
-
+            if (nums[index2]<nums[index1]){
+                swap(nums,index2,index1);
             }
-
+            sort(nums,index1+1,index2+1);
         }
-        return 0;
+    }
+    static void swap(int []nums , int index1 , int index2){
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
     }
 }
