37090b1bbc3cbca64033aec2f3e25a46cff84d13
Vignesh <vigneshgopi74@gmail.com>
2023-04-18 14:51:37 +0530

    counting paths in maze

--- src/Count_Paths_In_Maze.java
+++ src/Count_Paths_In_Maze.java
@@ -2,16 +2,13 @@ package src;
 
 public class Count_Paths_In_Maze {
     public static void main(String[] args) {
-        int m = 3; int n = 3;
-        System.out.println(countPaths(0,0,m,n));
+        System.out.println(countPaths(3,3));
     }
-    static int countPaths(int i , int j , int n, int m){
-        if (i == n || j == m)
-            return 0;
-        if (i == n-1 && j == m-1)
+    static int countPaths(int r , int c ){
+        if (r == 1 || c == 1)
             return 1;
-        int down = countPaths(i+1,j,n,m);
-        int right = countPaths(i,j+1,n,m);
+        int down = countPaths(r-1,c);
+        int right = countPaths(r,c-1);
         return down+right;
     }
 }
