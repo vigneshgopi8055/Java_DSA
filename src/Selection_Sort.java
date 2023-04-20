bc6ab4a7f42b699192943b37a64e43f5a94b0db3
Vignesh <vigneshgopi74@gmail.com>
2023-04-20 14:58:04 +0530

    selection sort

--- src/Selection_Sort.java
+++ src/Selection_Sort.java
@@ -7,26 +7,24 @@ public class Selection_Sort {
         int []arr = {3,1,5,4,2};
         sort(arr);
         System.out.println(Arrays.toString(arr));
-        System.out.println(maxindex(arr));
+
 
     }
     static void sort(int []nums){
         for (int i = 0; i < nums.length; i++) {
             int last = nums.length-i-1;
-            int max = maxindex(nums);
-
-            int temp = nums[max];
-            nums[max] = nums[last];
-            nums[last] = temp;
+            int max = maxIndex(nums,0,last);
+            int temp = nums[last];
+            nums[last] =nums[max];
+            nums[max] = temp;
         }
 
     }
-    static int maxindex(int []nums){
-        int max = nums[0];
-        for (int i = 1; i < nums.length; i++){
-            if (nums[i]>max){
+    static int maxIndex(int []nums, int start, int end){
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (nums[max]<nums[i])
                 max = i;
-            }
         }
         return max;
     }
