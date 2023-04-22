de13d42038a0bbf7cc777b68a5ee4128a77b9606
Vignesh <vigneshgopi74@gmail.com>
2023-04-22 14:40:35 +0530

    example1

---
+++ src/OOP2.java
@@ -0,0 +1,28 @@
+package src;
+//Write a program to print the area of a rectangle by creating a class named 'Area' having two methods.
+// First method named as 'setDim' takes length and breadth of rectangle as parameters and the second method named as 'getArea' returns the area of the rectangle.
+// Length and breadth of rectangle are entered through keyboard.
+public class OOP2 {
+    public static void main(String[] args) {
+        Area dimensions = new Area();
+        dimensions.length = 5;
+        dimensions.breadth = 6;
+        dimensions.getArea();
+        Area input = new Area(6,7);
+        input.getArea();
+    }
+    static class Area{
+        int length;
+        int breadth;
+         Area(int l , int b){
+            this.length = l;
+            this.breadth = b;
+        }
+        Area(){
+
+        }
+        void getArea(){
+            System.out.println(length*breadth);
+        }
+    }
+}
