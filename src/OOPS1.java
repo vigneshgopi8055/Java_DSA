4dbaf53c4cd558c940022a43324829127cd16c64
Vignesh <vigneshgopi74@gmail.com>
2023-04-22 13:42:20 +0530

    classes and objects

---
+++ src/OOPS1.java
@@ -0,0 +1,32 @@
+package src;
+//CLASSES AND OBJECTS
+public class OOPS1 {
+    public static void main(String[] args) {
+        car car1 = new car();  //car()->constructor
+        car1.name = "audi";    //car -> class
+        car1.color = "black";  //name,price,color -> properties of class
+        car1.price = 4000000;  //car1,car2->objects of class
+        car car2 = new car();
+        car2.name = "benz";
+        car2.color = "red";
+        car2.price = 5000000;
+        car1.printPrice();
+        car2.printName();
+        car1.Printcolor();
+    }
+    static class car{
+        String name;
+        String color;
+        int price;
+
+        void printName(){
+            System.out.println(this.name);
+        }
+        void printPrice(){
+            System.out.println(this.price);
+        }
+        void Printcolor(){
+            System.out.println(this.color);
+        }
+    }
+}
