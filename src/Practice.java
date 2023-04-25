c1c02d9d77385e0bc1203552241d3cf4c9341d3b
Vignesh <vigneshgopi74@gmail.com>
2023-04-25 15:05:03 +0530

    polymorphism

---
+++ src/Practice.java
@@ -0,0 +1,34 @@
+package src;
+
+public class Practice {
+    static public void main(String[] args) {
+        Shapes shape = new Shapes();
+        Circle circle = new Circle();
+        Square square = new Square();
+        Shapes triangle = new Triangle();
+        shape.Area();
+        circle.Area();
+        square.Area();
+        triangle.Area();
+    }
+    static class Shapes{
+        void Area(){
+            System.out.println("am in shapes");
+        }
+    }
+    static class Circle extends Shapes{
+        void Area(){
+            System.out.println("pi * r * r");
+        }
+    }
+    static class Square extends Shapes{
+        void Area(){
+            System.out.println("side * side");
+        }
+    }
+    static class Triangle extends Shapes{
+        void Area(){
+            System.out.println("0.5 * base * height");
+        }
+    }
+}
